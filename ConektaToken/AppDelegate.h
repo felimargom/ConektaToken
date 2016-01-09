d2b7071cc121358768bd34eb0b65322077e1f01d
Felicitas Martínez Gómez <FeliMarGom@macbook-air-de-elizabeth-2.local>
2016-01-09 00:35:51 -0600

    Initial Commit

---
+++ ConektaToken/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConektaToken
+//
+//  Created by Felicitas Martínez Gómez on 09/01/16.
+//  Copyright © 2016 Felicitas Martínez Gómez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
